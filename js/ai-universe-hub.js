f24d0248f480931771231b3b1695137f9aa9fc34
sakibhosen98 <sakibhosen9815@gmail.com>
2023-03-03 00:11:34 +0600

    added all card design

--- js/ai-universe-hub.js
+++ js/ai-universe-hub.js
@@ -6,6 +6,7 @@ const loadCards = () =>{
 
 const showCards = (cards) =>{
   const cardsContainer = document.getElementById('cards-container');
+  cards = cards.slice(0, 6)
   cards.forEach(card => {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('col');
@@ -13,13 +14,27 @@ const showCards = (cards) =>{
     <div class="card">
     <img src=${card.image} class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">${card.name}</h5>
-      <p class="card-text">${card.features}</p>
+      <h5 class="card-title m-0 p-0">Features</h5>
+      <ol class="p-3">
+        <li>${card.features[0]}</li>
+        <li>${card.features[1]}</li>
+        <li>${card.features[2]}</li>
+      </ol>
+      <hr>
+      <h4 class="">${card.name}</h4>
+      <div class="d-flex justify-content-between">
+        <div class="d-flex align-items-center">
+        <img src="./img/date.png" class="img-fluid" alt="..." height-"10" width="30">
+        <p>${card.published_in}</p>
+        </div>
+        <img src="./img/arrow.png" class="img-fluid" alt="..." height-"15" width="30">
+      </div>
     </div>
   </div>
     `
     cardsContainer.appendChild(cardDiv)
   });
+  
 }
 
 
