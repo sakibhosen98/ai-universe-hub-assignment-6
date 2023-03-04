07d04637ccf99479bb7d865e5a7835473408ee63
sakibhosen98 <sakibhosen9815@gmail.com>
2023-03-04 16:06:39 +0600

    added spinner

--- js/ai-universe-hub.js
+++ js/ai-universe-hub.js
@@ -1,23 +1,25 @@
 const loadCards = () =>{
   fetch('https://openapi.programming-hero.com/api/ai/tools')
   .then(res => res.json())
-  .then(data => showCards(data.data.tools))
+  .then(data => showCards(data.data.tools.slice(0, 6)))
+  toggleSpinner(true);
 }
 
 const showCards = (cards) =>{
   const cardsContainer = document.getElementById('cards-container');
 // Display 6 cards only
-const showMore = document.getElementById('show-more');
-if(cards.length > 6){
-  cards = cards.slice(0, 6);
-  showMore.classList.remove('d-none');
-}
-else{
-  showMore.classList.add('d-none')
-}
+// const showMore = document.getElementById('show-more');
+// if(cards.length > 6){
+//   cards = cards.slice(0, 6);
+//   showMore.classList.remove('d-none');
+// }
+// else{
+//   showMore.classList.add('d-none')
+// }
   cards.forEach(card => {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('col');
+
     cardDiv.innerHTML = `
     <div class="card">
     <img src=${card.image} class="card-img-top" alt="...">
@@ -42,6 +44,8 @@ else{
     `
     cardsContainer.appendChild(cardDiv)
   });
+
+  toggleSpinner(false);
 
   // stop spinner loader
   toggleSpinner(false)
@@ -69,7 +73,7 @@ const showCardDetail = cardDetail =>{
   const {description,image_link,features,input_output_examples,integrations,pricing} = cardDetail;
   console.log(cardDetail)
   const modalBody = document.getElementById('modal-body');
-
+  let integration = integrations.map(a=>(`<li>${a}</li>`)).join("");
   modalBody.innerHTML = `
     <div class="border p-3">
       <h6 class="fw-bold">${description}</h6>
@@ -87,17 +91,15 @@ const showCardDetail = cardDetail =>{
         <div>
           <h3>Features</h3>
             <ul>
-              <li>${features[1].feature_name}</li>
-              <li>${features[2].feature_name}</li>
-              <li>${features[3].feature_name}</li>
+            <li>${features[1].feature_name}</li>
+            <li>${features[2].feature_name}</li>
+            <li>${features[3].feature_name}</li> 
             </ul>
         </div>
         <div>
-          <h3>Integrations</h3>
+          <h3>integrations</h3>
           <ul>
-            <li>${integrations[0] ? integrations[0]: 'Data not found'}</li>
-            <li>${integrations[1] ? integrations[1]: 'Data not found'}</li>
-            <li>${integrations[2] ? integrations[0]: 'Data not found'}</li>
+          ${integration ? integration : 'data not found'};
           </ul>
         </div>
       </div>
@@ -106,7 +108,7 @@ const showCardDetail = cardDetail =>{
     <div>
     <div class="card positon-relative">
     <img src=${image_link[0]} class="card-img-top" alt="...">
-    <button type="button" class="btn btn-danger positon-absolute top-0 end-0" data-bs-dismiss="modal">Close</button>
+    <button type="button" class="btn btn-danger positon-absolute top-0 end-0" data-bs-dismiss="modal">acco</button>
     <div class="card-body">
       </h4 class="fw-bold fs-3">${input_output_examples[0].input}</h4>
       <p class="card-text">${input_output_examples[0].output}</p>
@@ -117,13 +119,6 @@ const showCardDetail = cardDetail =>{
   `
 }
 // -------------------------
-const processSearch = (datalimit) =>{
-   loadCards(datalimit)
-}
-
-// document.getElementById('btn-show-more').addEventListener('click', function(){
-//   toggleSpinner(true);
-// })
 
 const toggleSpinner = isLoading =>{
   const loaderSection = document.getElementById('loader');
@@ -136,12 +131,8 @@ const toggleSpinner = isLoading =>{
 }
 
 
-document.getElementById('btn-show-more').addEventListener('click', function(){
-  toggleSpinner(true);
-  processSearch()
-})
-
 loadCards();
+
 
 
 
